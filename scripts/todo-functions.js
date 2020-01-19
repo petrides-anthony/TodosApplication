3d7aaa4a514ca0a2012d997f53315a83b9fe7992
unknown <petrides.anthony@gmail.com>
2020-01-19 13:02:24 +0000

    Seperating Todo Application from TheModernJavascriptBootcamp

---
+++ scripts/todo-functions.js
@@ -0,0 +1,116 @@
+// Read existing todos from localStorage
+const getSavedTodos = () => {
+    const todosJSON = localStorage.getItem('todos')
+
+    try {
+        return todosJSON ? JSON.parse(todosJSON) : []
+    } catch (e) {
+        return []
+    }
+}
+
+// Save todos to localStorage
+const saveTodos = (todos) => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Render application todos based on filters
+const renderTodos = (todos, filters) => {
+    const todoEl = document.querySelector('#todos')
+    const filteredTodos = todos.filter((todo) => {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+        
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
+    
+    todoEl.innerHTML = ''
+    todoEl.appendChild(generateSummaryDOM(incompleteTodos))
+
+    if (filteredTodos.length > 0) {
+        filteredTodos.forEach((todo) => {
+            todoEl.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'No todos to show'
+        todoEl.appendChild(messageEl)
+    }
+
+}
+
+// Remove Todo from the list by id
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id)
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
+
+// Toggle the completed value for a given todo
+const toggleTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id)
+
+    if (todo) {
+        todo.completed = !todo.completed
+    }
+}
+
+// Get the DOM elements for an individual note
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const todoText = document.createElement('span')
+    const removeButton = document.createElement('button')
+
+    // Setup todo checkbox
+    checkbox.setAttribute('type', 'checkbox')
+    // Render completed todos with checked checkboxes
+    checkbox.checked = todo.completed
+    containerEl.appendChild(checkbox)
+    checkbox.addEventListener('change', () => {
+        toggleTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+
+    // Setup the todo text
+    todoText.textContent = todo.text
+    containerEl.appendChild(todoText)
+
+    // Setup container
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    // Setup todo remove button
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+
+    return todoEl
+}
+
+// Get the Summary DOM
+const generateSummaryDOM = (incompleteTodos) => {
+    const summary = document.createElement('h2')
+    summary.classList.add('list-title')
+
+    if (incompleteTodos.length > 1) {
+        summary.textContent = `You have ${incompleteTodos.length} todos left`
+    } else {
+        summary.textContent = `You have ${incompleteTodos.length} todo left`
+    }
+
+    return summary
+}
